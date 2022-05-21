c973b41d64706a2ecca85945e65facc4ef7d8566
Bratislav Ljubisic <bratislav.ljubisic@icliud.com>
2022-05-21 10:28:16 +0200

    Modified the correct order

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftyCloudKit",
-            path: "SwiftyCloudKit/Classes"
-            dependencies: [])
+            dependencies: [],
+            path: "SwiftyCloudKit/Classes")
+            
     ]
 )
